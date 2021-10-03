1c5e6a2fefb550cac9789e962ae7995291446ac8
Nitish Kumar <57797801+Nitish-web-nik@users.noreply.github.com>
2021-10-03 23:15:14 +0530

    Update jquery.Jcrop.js

--- js/jquery.Jcrop.js
+++ js/jquery.Jcrop.js
@@ -5,6 +5,7 @@
  * http://github.com/tapmodo/Jcrop
  * Copyright (c) 2008-2013 Tapmodo Interactive LLC {{{
  *
+ 
  * Permission is hereby granted, free of charge, to any person
  * obtaining a copy of this software and associated documentation
  * files (the "Software"), to deal in the Software without
